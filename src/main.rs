50c87d60213ba5d224e5ee1b12eb877a60a99e14
leonardovee <leoveeph@gmail.com>
2024-07-13 13:18:11 -0300

    parse the url

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{
-    io::{BufReader, Write},
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -19,7 +19,24 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let _ = BufReader::new(&mut stream);
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let reader = BufReader::new(&mut stream);
+    let http_request: Vec<String> = reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let endpoint = http_request
+        .first()
+        .unwrap()
+        .split_whitespace()
+        .nth(1)
+        .unwrap();
+
+    let response = match endpoint {
+        "/" => "HTTP/1.1 200 OK\r\n\r\n",
+        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    };
+
     stream.write_all(response.as_bytes()).unwrap();
 }
