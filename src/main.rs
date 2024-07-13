a80c2778d1aecfe1399a1b014d322d1320c8b00a
leonardovee <leoveeph@gmail.com>
2024-07-13 12:17:42 -0300

    returns 200

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
-use std::net::TcpListener;
+use std::{
+    io::{BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(stream) => {
+                handle_connection(stream);
                 println!("accepted new connection");
             }
             Err(e) => {
@@ -12,4 +16,10 @@ fn main() {
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let _ = BufReader::new(&mut stream);
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
 }
